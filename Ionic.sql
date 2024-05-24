d22b8f8387afe79f8f01a55eaeabbd43321b12ef
heliton Vieira <helitondba@gmail.com>
2024-05-24 15:41:50 -0300

    primeiro commit full

---
+++ Ionic.sql
@@ -0,0 +1,14 @@
+web.telegram.org/fabricadecodigogrupo
+
+@fabricadecodigo
+
+Rodar servidor ionic:
+- ionic serve
+
+Iniciar um novo projeto:
+-- em branco => ionic start nomeProjeto blank;
+-- com header e footer => ionic start nomeProjeto tabs;
+-- com sidemenu => ionic start nomeProjeto sidemenu;
+
+Criar uma nova página:
+- ionic g page nome-da-pagina
